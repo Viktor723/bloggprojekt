7965a9ce240748bf120d6a2492733a55f94f5710
viktor723 <viktor.ramic.simic@abbgymnasiet.se>
2022-04-16 13:12:19 +0200

    google form

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,15 +1,23 @@
 <template>
-    <form action="https://public.herotofu.com/v1/17ad4720-b198-11ec-85ec-4bc9f7f36ae9" method="post">
-  <div>
-    <label for="name">Your Name</label>
-    <input name="Name" id="name" type="text" required />
-  </div>
-  <div>
-    <label for="email">Your Email</label>
-    <input name="Email" id="email" type="email" required  />
-  </div>
-  <div>
-    <input type="submit" value="Download CTA" />
-  </div>
-</form>
+<div class="googleform">
+<iframe 
+src="https://docs.google.com/forms/d/e/1FAIpQLSeHSU--MwIdd-qVojU7XQMtawQkdZd42psHX6agA_im_Zblbw/viewform?embedded=true" 
+    width="100%" 
+    height="1000" 
+    frameborder="0" 
+    marginheight="100px" 
+    marginwidth="0"
+    maringtop="10px"
+    onload="loaded()">
+        Loading...
+</iframe>
+></div>
+
 </template>
+
+<style>
+.googleform{
+ padding: 70px;
+
+}
+</style>
