fed4415a95f496a975898e4b2c7e48f01f9c4500
viktor723 <viktor.ramic.simic@abbgymnasiet.se>
2022-04-16 12:28:26 +0200

    h

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <form action="https://public.herotofu.com/v1/17ad4720-b198-11ec-85ec-4bc9f7f36ae9" method="post">
+  <div>
+    <label for="name">Your Name</label>
+    <input name="Name" id="name" type="text" required />
+  </div>
+  <div>
+    <label for="email">Your Email</label>
+    <input name="Email" id="email" type="email" required  />
+  </div>
+  <div>
+    <input type="submit" value="Download CTA" />
+  </div>
+</form>
 </template>
